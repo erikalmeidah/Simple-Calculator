63b8685706f5ec604fc059b7467714a2b4f42df2
Erik Companhone <e.comp2712@gmail.com>
2025-01-02 13:58:36 -0500

    done with sqrt and +/-. Using eval to calculate string.

--- calculator.js
+++ calculator.js
@@ -12,10 +12,11 @@ function updateDisplay(displayString) {
 function evalDisplay() {
   // get string to calculate
   toCalc = document.querySelector('.display').innerHTML;
-  toCalc = toCalc.replace('x', '*').replace('÷', '/').replace('√', 'Math.sqrt');
+  toCalc = toCalc.replace('x', '*').replace('÷', '/').replace('√', 'Math.sqrt(');
+  toCalc = processSqrt(toCalc);
   // calculate
-  let res = Function('return ' + toCalc)();
-  res = parseFloat(res.toFixed(2));
+  let res = eval(toCalc)
+  res = parseFloat(res.toFixed(2)); // wont return trailing zeros
   // display the result
   document.querySelector('.display').innerHTML = res;
 }
@@ -23,6 +24,27 @@ function evalDisplay() {
 // reset display
 function resetDisplay() {
   document.querySelector('.display').innerHTML = '';
+}
+
+// sqrt helper
+function processSqrt(toCalc) {
+  // loop through string
+  for (let i = 0; i < toCalc.length; i += 1) {
+    // if 'M' found, add 10 to i ('Math.sqrt(' offset))
+    if (toCalc[i] === 'M') {
+      i += 10;
+      // loop until not in same number or end of string
+      while (i <= toCalc.length) {
+        // add )
+        if (!'0123456789.'.includes(toCalc[i])) {
+          toCalc = toCalc.slice(0, i) + ')' + toCalc.slice(i);
+          break;
+        }
+        i += 1;
+      }
+    }
+  }
+  return toCalc;
 }
 
 // Number buttons
@@ -38,8 +60,29 @@ function resetDisplay() {
           updateDisplay(num);
           break;
         case '+/-':
-          // TODO
-          // reverse loop through display string until you find beginning or operation sign, then add - in front
+          // get display string
+          toCalc = document.querySelector('.display').innerHTML.trim();
+          
+           // reverse loop through the string
+          for (let i = toCalc.length - 1; i >= 0; i -= 1) {
+            if (!'0123456789.'.includes(toCalc[i])) {
+              //looped through last number, replace sign
+              switch (toCalc[i]) {
+                case '+':
+                  toCalc = toCalc.slice(0, i) + '-' + toCalc.slice(i + 1);
+                  break;
+                case '-':
+                  toCalc = toCalc.slice(0, i) + '+' + toCalc.slice(i + 1);
+                  break;
+                default:
+                  toCalc = toCalc.slice(0, i + 1) + '-' + toCalc.slice(i + 1);
+                  break;
+              }
+              break;
+            }
+          }
+          resetDisplay();
+          updateDisplay(toCalc);
           break;
         default:
           updateDisplay(num);
@@ -59,9 +102,6 @@ buttons = document.querySelectorAll('.round-button').forEach((button) => {
         break;
       case 'CE':
         updateDisplay('CE');
-        break;
-      case '√':
-        //TODO
         break;
       default:
         updateDisplay(operation);
